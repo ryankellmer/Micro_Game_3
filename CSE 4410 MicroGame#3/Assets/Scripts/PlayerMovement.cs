fcaa280b924035ea2bbd767175b6661c3b4dd3e6
Ryan Kellmer <ryankellmer@gmail.com>
2021-04-17 22:53:45 -0700

    Commit 2
    Added: Movement, Better Camera which follows player, and started on the map layout.

---
+++ CSE 4410 MicroGame#3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+        public PlayerController controller;
+        public float runSpeed = 40f;
+        float horizontalMovement = 0f;
+        bool jump = false;
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+               horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+               if(Input.GetButtonDown("Jump"))
+               {
+                   jump = true;
+               }
+        }
+
+    void FixedUpdate ()
+    {
+                controller.Move(horizontalMovement * Time.fixedDeltaTime, false, jump);
+                jump = false;
+    }
+}
